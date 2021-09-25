8d07a17e8c0bdfe7ebf7dbb10c18ce7b08de507d
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2021-09-25 21:14:39 +0200

    Game window 2 (dinner time ig)

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Game extends JFrame {
     RockPaperScissors game=new RockPaperScissors();
 
+    JButton Rock=new JButton();
+    JButton Paper= new JButton();
+    JButton Scissors= new JButton();
+    JButton End=new JButton("End Game");
+    JLabel Computer= new JLabel();
+    JLabel Scores= new JLabel();
+    JPanel ComputerPanel= new JPanel();
+    JPanel PlayerPanel = new JPanel();
+    JPanel Options=new JPanel();
+
+    public Game(){
+        Container cp= getContentPane();
+
+
+    }
 }
