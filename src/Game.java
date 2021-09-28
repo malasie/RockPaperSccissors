8796661f31cfcaf134bcfadee64a681354e07aa3
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2021-09-28 19:25:30 +0200

    Some more1

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game extends JFrame {
     RockPaperScissors game=new RockPaperScissors();
@@ -8,8 +10,11 @@ public class Game extends JFrame {
     JButton Paper= new JButton("P");
     JButton Scissors= new JButton("S");
     JButton End=new JButton("End Game");
-    JLabel Computer= new JLabel();
-    JLabel Scores= new JLabel();
+    JLabel Computer= new JLabel("Computer's move");
+    JLabel RoundWinner= new JLabel("Winner");
+    JLabel Scores= new JLabel("Score");
+    JLabel Title= new JLabel("Rock Paper Scissors");
+    JLabel Choice= new JLabel();
     JPanel Ranking = new JPanel();
     JPanel ComputerPanel= new JPanel();
     JPanel Info = new JPanel();
@@ -19,6 +24,7 @@ public class Game extends JFrame {
     public Game(){
         Container cp= getContentPane();
         cp.setLayout(new GridLayout(5,1));
+        cp.add(Title);
 
         cp.add(ComputerPanel);
         ComputerPanel.setLayout(new GridLayout(1,3));
@@ -27,17 +33,21 @@ public class Game extends JFrame {
 
         cp.add(Ranking);
         Ranking.setLayout(new GridLayout(2,1));
+        Ranking.add(RoundWinner);
         Ranking.add(Scores);
 
         cp.add(PlayerPanel);
         PlayerPanel.setLayout(new GridLayout(1,3));
         PlayerPanel.add(Rock);
+        Rock.addActionListener(new Rock_Move());
         PlayerPanel.add(Paper);
+        Paper.addActionListener(new Paper_Move());
         PlayerPanel.add(Scissors);
+        Scissors.addActionListener(new Scissors_Move());
 
         cp.add(Info);
         Info.setLayout(new GridLayout(4,1));
-        Info.add(new JLabel("Your choice:"));
+        Info.add(Choice);
         Info.add(new JLabel("Choose your move"));
         Info.add(new JLabel());
         Info.add(Options);
@@ -46,7 +56,73 @@ public class Game extends JFrame {
         Options.add(new JLabel());
         Options.add(End);
 
-
-
     }
+    class Rock_Move implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            game.player=0;
+            Choice.setText("Your choice: Rock");
+            game.game();
+            switch (game.winner) {
+                case 0:
+                    RoundWinner.setText("You Lost");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 1:
+                    RoundWinner.setText("Remis");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 2:
+                    RoundWinner.setText("You Won!");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+            }
+        }
+    }
+    class Paper_Move implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            game.player = 1;
+            Choice.setText("Your choice: Paper");
+            game.game();
+            switch (game.winner) {
+                case 0:
+                    RoundWinner.setText("You Lost");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 1:
+                    RoundWinner.setText("Remis");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 2:
+                    RoundWinner.setText("You Won!");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+            }
+        }
+    }
+    class Scissors_Move implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            game.player = 2;
+            Choice.setText("Your choice: Scissors");
+            game.game();
+            switch (game.winner) {
+                case 0:
+                    RoundWinner.setText("You Lost");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 1:
+                    RoundWinner.setText("Remis");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+                case 2:
+                    RoundWinner.setText("You Won!");
+                    Scores.setText(" Score:\nYou : Computer \n " + game.won + ":" + game.lost);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new Game();
+        f.setSize(600, 800);
+        f.setLocation(100, 50);
+        f.setVisible(true);
+    }
+
 }
--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -3,34 +3,33 @@ import java.util.Random;
 
 public class RockPaperScissors {
     int player;
+    String computer_move;
     int lost=0;
     int won=0;
+    int winner; //you lost= 0; remis=1; you won=2;
     Random rand= new Random();
     // 0 - rock; 1-paper; 2-scissors
-    void game(int player){
+    void game(){
         int computer= rand.nextInt(3);
+        switch (computer) {
+            case 0:
+                computer_move = "Rock";
+            case 1:
+                computer_move = "Paper";
+            case 2:
+                computer_move = "Scissors";
+        }
         if (computer==player){
-            System.out.println("REMIS");
-            System.out.println(" Score:\nYou : Computer \n "+won+":"+lost);
+            winner=1;
         }
         else if (player-computer==1 || player-computer==-2){
-            System.out.println("YOU WON!");
             won++;
-            System.out.println(" Score:\nYou : Computer \n "+won+":"+lost);
+            winner=2;
         }
         else {
-            System.out.println("YOU LOST");
             lost++;
-            System.out.println(" Score:\nYou : Computer \n "+won+":"+lost);
+            winner=0;
         }
 
-    }
-
-    public static void main(String[] a){
-        RockPaperScissors r=new RockPaperScissors();
-        r.game(2);
-        r.game(1);
-        r.game(2);
-        r.game(0);
     }
 }
